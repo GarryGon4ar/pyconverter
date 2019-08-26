aceba4197fa5cdba4c780ccc702de3e0af560cf7
GarryGon4ar <garrygon4ar@gmail.com>
2019-08-26 11:23:52 +0600

    Add mysql and sqlalchemy

--- models.py
+++ models.py
@@ -1,25 +1,38 @@
-
+import datetime
+import sqlalchemy
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy_utils import EmailType
 
-engine = create_engine("mysql+pymysql://fox:malder'@localhost:3306/converter",echo=True)
+engine = create_engine("mysql+mysqlconnector://fox:malder@localhost:3306/converter",echo=True)
 
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Youtube mp3 simple converter</title>
-</head>
-<style>
-    form {
-        text-align: center;
-    }
-</style>
-<body>
-<form action="" method="post">
-    {% csrf_token %}
-    {{ form }}
-    <input type="submit" value="Submit">
 
-  </form>
-</body>
-</html> -->
+Base = declarative_base()
+Session = sqlalchemy.orm.sessionmaker()
+Session.configure(bind=engine)
+session = Session()
+
+
+class Song(Base):
+    __tablename__ = 'songs'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    link = sqlalchemy.Column(sqlalchemy.VARCHAR(length=512))
+    email_address = sqlalchemy.Column(EmailType)
+    created_at = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.utcnow)
+    def __str__(self):
+        return self.link
+
+    def __repr__(self):
+        return "<User(name='{0}', fullname='{1}', nickname='{2}')>".format(
+                            self.name, self.fullname, self.nickname)
+Base.metadata.create_all(engine)
+
+def create_query(url, email):
+    query = Song(link=url, email_address=email)
+    session.add(query)
+    session.commit()
+
+    return print(f"Song(url={url}, email={email})")
+
+
--- views.py
+++ views.py
@@ -1,10 +1,15 @@
 from render import render_template
 from tasks import download
+from models import Song, create_query
 
 def index(request, response):
 	if request.method == "POST":
 		url = request.POST.get('url')
 		email = request.POST.get('email')
+		# song = Song(link=url)
+		# session.add(song)
+		# session.commit()
+		create_query(url, email)
 		download.delay(url, email)
 		response.text = "Вам будет отправлено письмо на email"
 		return response
